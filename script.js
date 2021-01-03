15140ac3a1dece249cc81c5cdd8181630fc49594
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-03 05:37:35 -0500

    adjust the width of each frame along the horizontal scroll.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 $("#horizontal_wrap").wrapInner("<table cellspacing='10'><tr>");
 $(".segment").wrap("<td></td>");
+
+var section = document.querySelectorAll('section');
+var x = screen.width;
+var b = x.toString();
+b += 'px';
+section[0].style.width = 'b';
+console.log(b);
