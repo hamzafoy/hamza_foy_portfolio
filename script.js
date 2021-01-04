3ec6484c49c3128b0c95dbf530c4b1f827143dbe
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-03 21:22:00 -0500

    adjust each section on the horizontal scroll to be the width of the viewport screen.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $("#horizontal_wrap").wrapInner("<table cellspacing='10'><tr>");
 $(".segment").wrap("<td></td>");
 
-var section = document.querySelectorAll('section');
+var sections = document.querySelectorAll('section');
 var x = screen.width;
 var b = `${x.toString()}px`;
-section[0].style.width = b;
+for (let i = 0; i < sections.length; i++) {
+ let section = sections[i];
+ section.style.width = b;
+}
