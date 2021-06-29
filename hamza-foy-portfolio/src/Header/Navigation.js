e01ae070a79fb5d96caec3f795591e98993a6933
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-28 23:44:04 -0400

    add an additional conditional to the navbar component, this time showing various 'titles' for me.

--- hamza-foy-portfolio/src/Header/Navigation.js
+++ hamza-foy-portfolio/src/Header/Navigation.js
@@ -3,16 +3,17 @@ import './Navigation.css'
 
 const webDeveloper = {
     name: 'Hamza Foy',
-    title: 'Freelance Web Developer'
+    title: ['Freelance Web Developer', 'Loving Husband', 'Respected Community Member', 'Junior Full-Stack Engineer']
 }
 
 class Navigation extends React.Component {
     render() {
         const dayAndNight = new Date().getHours();
+        let randomNum = Math.floor((Math.random() * 4));
         return (
             <div className="header-container">
                 <section className="author">
-                    {webDeveloper.name} &#9876; {webDeveloper.title} by {dayAndNight >= 12 ? 'day' : 'night'}
+                    {webDeveloper.name} &#9876; {webDeveloper.title[randomNum]} by {dayAndNight >= 12 ? 'day' : 'night'}
                 </section>
             </div>
         )
