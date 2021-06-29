3ba001e8bd4eb53c00f7b8eb96a5107871f925fe
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-29 05:14:12 -0400

    add a font for the navbar, web design logo, and work with alignment of elements in the navbar.

--- hamza-foy-portfolio/src/Header/Navigation.js
+++ hamza-foy-portfolio/src/Header/Navigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import './Navigation.css'
+import './Navigation.css';
+import logo from './hamza_Foy_Logo.png'
 
 const webDeveloper = {
     name: 'Hamza Foy',
+    logo: './hamza_Foy_Logo.png',
     title: ['Freelance Web Developer', 'Loving Husband', 'Respected Community Member', 'Junior Full-Stack Engineer']
 }
 
@@ -12,8 +14,9 @@ class Navigation extends React.Component {
         let randomNum = Math.floor((Math.random() * 4));
         return (
             <div className="header-container">
+                <img className="logo" src={logo} alt="Hamza Foy Web Design"/>
                 <section className="author">
-                    {webDeveloper.name} &#9876; {webDeveloper.title[randomNum]} by {dayAndNight >= 12 ? 'day' : 'night'}
+                    {webDeveloper.name} &#9876; {webDeveloper.title[randomNum]} by {dayAndNight >= 8 && dayAndNight <= 20 ? 'day' : 'night'}
                 </section>
             </div>
         )
