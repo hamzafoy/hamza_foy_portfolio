3426432a072a812fb2f0efb31528176e7297757d
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-30 04:43:50 -0400

    fix the flexbos of the navbar with the linkedin, github icons accounted for.

--- hamza-foy-portfolio/src/Header/Navigation.js
+++ hamza-foy-portfolio/src/Header/Navigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Navigation.css';
 import logo from './hamza_Foy_Logo.png'
+import linkedin from './linkedin.svg'
+import github from './github.svg'
 
 const webDeveloper = {
     name: 'Hamza Foy',
@@ -14,9 +16,16 @@ class Navigation extends React.Component {
         let randomNum = Math.floor((Math.random() * 4));
         return (
             <div className="header-container">
-                <img className="logo" src={logo} alt="Hamza Foy Web Design"/>
+                
                 <section className="author">
+                    <img className="logo" src={logo} alt="Hamza Foy Web Design"/>
+                    <aside>
                     {webDeveloper.name} &#9876; {webDeveloper.title[randomNum]} by {dayAndNight >= 8 && dayAndNight <= 20 ? 'day' : 'night'}
+                    </aside>
+                </section>
+                <section className="icon-list">
+                    <img className="icon" src={github} alt="Hamza's Github"/>
+                    <img className="icon" src={linkedin} alt="Hamza's LinkedIn"/>
                 </section>
             </div>
         )
