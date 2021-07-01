3d38b1050e99cf48389b03b9ad6804afd1d772af
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-01 16:10:37 -0400

    add a range-slider input to allow user to select a date range to see my works in chronological order.

--- hamza-foy-portfolio/src/Timeline/Timeline.js
+++ hamza-foy-portfolio/src/Timeline/Timeline.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Timeline.css';
+import './Timeline-Handler.css'
 
 class Timeline extends React.Component {
     render() {
@@ -13,8 +14,15 @@ class Timeline extends React.Component {
                     </h1>
 
                     <p>
-                    This is a test
+                    My compilations of websites and apps,<br/>from humblest beginnings to now. . .
                     </p>
+
+                    <aside className="timeline-handler">
+                        <input type="range" id="timeline-range" name="timeline-range"
+                                min="0" max="5" value="0"/>
+                        <label for="timeline-range">From earliest to greatest</label>
+                    </aside>
+                    
                     
                 </section>
 
