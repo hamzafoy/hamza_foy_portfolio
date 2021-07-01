358d0d7dc74b2629b3c86a83e8030eee1553bab1
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-01 17:06:09 -0400

    adjust the timeline input-range handler.

--- hamza-foy-portfolio/src/Timeline/Timeline.js
+++ hamza-foy-portfolio/src/Timeline/Timeline.js
@@ -3,6 +3,17 @@ import './Timeline.css';
 import './Timeline-Handler.css'
 
 class Timeline extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: '0'};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        //alert(this.state.value)
+        this.setState({value: event.target.value});
+    }
+
     render() {
         return(
             <div className="timeline-container">
@@ -19,7 +30,7 @@ class Timeline extends React.Component {
 
                     <aside className="timeline-handler">
                         <input type="range" id="timeline-range" name="timeline-range"
-                                min="0" max="5" value="0"/>
+                                min="0" max="5" defaultValue="0" onChange={this.handleChange}/>
                         <label for="timeline-range">From earliest to greatest</label>
                     </aside>
                     
