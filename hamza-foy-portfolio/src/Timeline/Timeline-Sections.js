f8ced9f226b94011ce837c176e1b7845570fcc61
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-03 00:42:13 -0400

    add an additional timeline section, add a switch statement to handle displaying the sections based on the range slider, and modularize the Timeline app by placing the timeline sections in an object in a separate file that is imported/exported.

---
+++ hamza-foy-portfolio/src/Timeline/Timeline-Sections.js
@@ -0,0 +1,30 @@
+const timelineSections = {
+    itemOne: (
+        <section className="timeline-display-one">
+            <h1>
+                Volume I: In the beginning. . .
+            </h1>
+
+            <p>
+                In the beginning, there was light on the screen and code written on it.
+                First, I learned HTML - that structure rendered in my browser.
+                Then, I learned CSS - the rendered code in my browser filled with color and style.
+            </p>
+        </section>
+    ),
+    itemTwo: (
+        <section className="timeline-display-two">
+            <h1>
+                Volume II: It doesn't look like anything to me. . .
+            </h1>
+
+            <p>
+                As I explored HTML and CSS, in the hopes of designing visually pleasing websites,
+                I came to realize that the structure and the style did not interact or move.
+                I set out to learn JavaScript and loved the simple syntax and the logic it encouraged.
+            </p>
+        </section>
+    )
+}
+
+export default timelineSections;
--- hamza-foy-portfolio/src/Timeline/Timeline.js
+++ hamza-foy-portfolio/src/Timeline/Timeline.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import './Timeline.css';
 import './Timeline-Handler.css'
+import timelineSections from './Timeline-Sections'
 
-const timelineSections = {
-    itemOne: (
-        <section className="timeline-display-one">
-            <h1>
-                Volume I: In the beginning. . .
-            </h1>
-
-            <p>
-                In the beginning, there was light on the screen and code written on it.
-                First, I learned HTML - that structure rendered in my browser.
-                Then, I learned CSS - the rendered code in my browser filled with color and style.
-            </p>
-        </section>
-    )
-}
 
 class Timeline extends React.Component {
     constructor(props) {
@@ -32,9 +18,16 @@ class Timeline extends React.Component {
 
     render() {
         let timelineDisplay;
-        if (this.state.value === '1') {
-            timelineDisplay = timelineSections.itemOne;
+        
+        switch (this.state.value) {
+            case '1':
+                timelineDisplay = timelineSections.itemOne;
+                break;
+            case '2':
+                timelineDisplay = timelineSections.itemTwo;
+                break;
         }
+
         return(
             <div className="timeline-container">
 
