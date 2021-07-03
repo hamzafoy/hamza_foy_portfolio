4af6b6fd81b6574c6ba4e00dcf2651446076f93a
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-02 23:38:51 -0400

    begin to flesh out one of the timeline sections that will be displayed depending on the position of the knob on the range slider.

--- hamza-foy-portfolio/src/Timeline/Timeline.js
+++ hamza-foy-portfolio/src/Timeline/Timeline.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Timeline.css';
 import './Timeline-Handler.css'
 
+const timelineObject = {
+    itemOne: (
+        <section>
+            This is a test
+        </section>
+    )
+}
+
 class Timeline extends React.Component {
     constructor(props) {
         super(props);
@@ -33,7 +41,18 @@ class Timeline extends React.Component {
                                 min="0" max="5" defaultValue="0" onChange={this.handleChange}/>
                         <label for="timeline-range">From earliest to greatest</label>
                     </aside>
-                    
+
+                    <section className="timeline-display-one">
+                        <h1>
+                            Volume I: In the beginning. . .
+                        </h1>
+
+                        <p>
+                            In the beginning, there was light on the screen and code written on it.
+                            First, I learned HTML - that structure rendered in my browser.
+                            Then, I learned CSS - the rendered code in my browser filled with color and style.
+                        </p>
+                    </section>                    
                     
                 </section>
 
