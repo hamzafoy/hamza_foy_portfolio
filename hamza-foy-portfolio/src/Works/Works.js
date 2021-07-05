dbbe684fb526dee30c0a4ecc30d50a4b49e9fbf5
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-05 18:02:46 -0400

    start work on 2nd project to showcase.

--- hamza-foy-portfolio/src/Works/Works.js
+++ hamza-foy-portfolio/src/Works/Works.js
@@ -5,6 +5,7 @@ import css3 from './img/css3_2.svg'
 import html5 from './img/html.svg'
 import javascript from './img/javascript_3.svg'
 import surgesh from './img/surge-sh.svg'
+import mdevent from './img/mdevent.png'
 
 class Works extends React.Component {
     render() {
@@ -32,9 +33,29 @@ class Works extends React.Component {
                             </p>
                         </section>
 
-                        <img className="kebab-wheels-foto" src={kebab} alt="Kebab on Wheels homepage"/>
+                        <img className="project-fotos" src={kebab} alt="Kebab on Wheels homepage"/>
 
                     </div>
+
+                    <div className="works-section">
+
+                        <section className="techs-primer">
+                            <aside className="tech-svg-container">
+                                <img className="tech-icon" src={css3}/>
+                                <img className="tech-icon" src={html5}/>
+                                <img className="tech-icon" src={javascript}/>
+                                <img className="tech-icon" src={surgesh}/>
+                            </aside>
+                            <p>
+                                This website, developed for Abdullah Mahmoud, for the Kebab on Wheels food truck utilized
+                                Surge.sh SSG to push updates requested by the client onto the domain in a swift fashion.
+                            </p>
+                        </section>
+
+                        <img className="project-fotos" src={mdevent} alt="MD Event Planner homepage"/>
+
+                    </div>
+
 
                 </section>
 
