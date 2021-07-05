2a51e3acb31f3029d279689d41b111efeda20344
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-05 04:03:06 -0400

    begin work on the Works (cited) Component to display previous projects in an appealing and intelligent manner.

--- hamza-foy-portfolio/src/App.js
+++ hamza-foy-portfolio/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Timeline from './Timeline/Timeline'
+import Timeline from './Timeline/Timeline';
+import Works from './Works/Works'
 
 function App() {
   return (
     <div className="App">
       <Timeline/>
-      <Timeline/>
+      <Works/>
     </div>
   );
 }
---
+++ hamza-foy-portfolio/src/Works/Works.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Works.css';
+
+class Works extends React.Component {
+    render() {
+        return(
+            <div className="works-container">
+
+                <section className="works-cited">
+
+                </section>
+                
+            </div>
+        )
+    }
+}
+
+export default Works;
