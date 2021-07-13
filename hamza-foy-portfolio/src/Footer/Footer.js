8ead0554994dfb13bb0d11f1aad55b3c360774f3
Ross Foy <hamzaoflouisville@gmail.com>
2021-07-12 20:29:06 -0400

    add Whatsapp and Gmail icons to footer for people to reach out to me.

--- hamza-foy-portfolio/src/Footer/Footer.js
+++ hamza-foy-portfolio/src/Footer/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link, animateScroll } from 'react-scroll'
 import './Footer.css'
 import upicon from './up_arrow.svg'
+import email from './gmail.svg'
+import whatsapp from './whatsapp.svg'
 
 class Footer extends React.Component {
     scrollToTop = () => {
@@ -12,7 +14,12 @@ class Footer extends React.Component {
         return(
             <div className="footer-container">
                 <h2>
-                    Hamza Foy - Web Designer, Programmer
+                    Hamza Foy - Web Designer, Programmer - 
+                    Reach me:
+                    <div className="footer-icons">
+                        <img className="contact" src={email} alt="Gmail" title="Email me!"/>
+                        <img className="contact" src={whatsapp} alt="Whatsapp" title="Whatsapp me!"/>
+                    </div>
                 </h2>
                 <Link
                 to="react-scroll-top"
